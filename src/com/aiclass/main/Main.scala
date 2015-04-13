ffbfc1e5bd72182ddec886ba90081622fc7b9831
azranel <chunkyo@gmail.com>
2015-04-13 14:41:13 +0200

    Init project commit

---
+++ src/com/aiclass/main/Main.scala
@@ -0,0 +1,10 @@
+package com.aiclass.main
+
+/**
+ * Created by azranel on 4/13/15.
+ */
+object Main {
+  def main (args: Array[String]) {
+    println("Ala ma kota i sztuczną inteligencję");
+  }
+}

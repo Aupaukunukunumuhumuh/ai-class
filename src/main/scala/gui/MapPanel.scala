78995e37280bccee42583dc8ca959396e2fc1617
azranel <chunkyo@gmail.com>
2015-05-03 21:58:53 +0200

    Some code cleanup

--- src/main/scala/gui/MapPanel.scala
+++ src/main/scala/gui/MapPanel.scala
@@ -3,7 +3,8 @@ package gui
 import java.awt.{Graphics2D, Color, Dimension}
 import java.io.File
 import javax.imageio.ImageIO
-import logic.{CellObject, Cell}
+
+import logic.Cell
 
 import scala.swing.{Swing, Panel}
 
@@ -11,10 +12,13 @@ import scala.swing.{Swing, Panel}
  * Created by azranel on 4/27/15.
  */
 class MapPanel extends Panel {
-  maximumSize = new Dimension(logic.Map.MAP_SIZE*logic.CellObject.CELL_SIZE,
-                              logic.Map.MAP_SIZE*logic.CellObject.CELL_SIZE)
+  private val CELL_SIZE = 32
+
+  maximumSize = new Dimension(logic.Map.MAP_SIZE*CELL_SIZE,
+                              logic.Map.MAP_SIZE*CELL_SIZE)
   background = new Color(0, 0, 0)
   border = Swing.EmptyBorder(10, 10, 10, 10)
+
 
   override def paintComponent(g: Graphics2D): Unit = {
     //All drawing is done here
@@ -23,7 +27,7 @@ class MapPanel extends Panel {
 
   private def drawTile(g : Graphics2D, tile : Cell): Unit = {
     val image = ImageIO.read(tile.image)
-    g.drawImage(image, tile.x*CellObject.CELL_SIZE,
-      tile.y*CellObject.CELL_SIZE, null)
+    g.drawImage(image, tile.x*CELL_SIZE,
+      tile.y*CELL_SIZE, null)
   }
 }
--- src/main/scala/logic/Cell.scala
+++ src/main/scala/logic/Cell.scala
@@ -7,11 +7,10 @@ import scala.io.Source
 /**
  * Created by azranel on 4/28/15.
  */
-class Cell(val x : Int, val y :Int, val isObstacle : Boolean = false, var image : File = CellObject.GRASS_FILE)  {
+class Cell(val x : Int, val y :Int, val isObstacle : Boolean = false, var image : File = Cell.GRASS_FILE)  {
 }
 
-object CellObject {
-  val CELL_SIZE = 32
+object Cell {
   val GRASS_FILE = new File("./resources/green2.jpg")
   val SMALL_GRASS_FILE = new File("./resources/green.jpg")
 }

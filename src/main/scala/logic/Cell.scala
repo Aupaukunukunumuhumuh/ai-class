e9327d11db919e1d0038343351977ab8df1e89ad
azranel <chunkyo@gmail.com>
2015-05-11 12:18:27 +0200

    Added more sophisticated map

--- src/main/scala/logic/Cell.scala
+++ src/main/scala/logic/Cell.scala
@@ -12,5 +12,11 @@ class Cell(val x : Int, val y :Int, val isObstacle : Boolean = false, var image 
 
 object Cell {
   val GRASS_FILE = new File("./resources/green2.jpg")
-  val SMALL_GRASS_FILE = new File("./resources/green.jpg")
+  val WALL_FILE = new File("./resources/wall.png")
+  val CELL_TYPES = List("0", "1")
+
+  def isCellType(s : String): Boolean = {
+    return CELL_TYPES.contains(s)
+  }
+
 }
--- src/main/scala/logic/Map.scala
+++ src/main/scala/logic/Map.scala
@@ -2,15 +2,39 @@ package logic
 
 import gui.MapPanel
 
+import scala.collection.mutable.ListBuffer
+import scala.io.Source
+
 /**
  * Created by azranel on 4/28/15.
  */
 class Map(val mapPanel : MapPanel) {
   val board = Array.ofDim[Cell](Map.MAP_SIZE, Map.MAP_SIZE)
 
-  for(xs <- 0 to Map.MAP_SIZE-1; ys <- 0 to Map.MAP_SIZE-1) { board(xs)(ys) = new Cell(xs, ys) }
+  Map.initializeMap(board)
 }
 
 object Map {
   val MAP_SIZE = 15
+  val MAP_FILE_PATH = "./resources/map.txt"
+
+  def initializeMap(board : Array[Array[Cell]]): Unit = {
+    var cellTypesList = new ListBuffer[String]
+    Source.fromFile(MAP_FILE_PATH).foreach(c => cellTypesList += c.toString)
+    val filteredCellTypesList = cellTypesList.filter(s => Cell.isCellType(s)).map(s => s.toInt).toList
+    fillMapWithCellObjects(board, filteredCellTypesList)
+  }
+  private def fillMapWithCellObjects(board : Array[Array[Cell]], types : List[Int]): Unit = {
+    var (x, y) = (0, 0)
+    for(cellType <- types) {
+      board(x)(y) = cellType match {
+        case 0 => new Cell(x, y)
+        case 1 => new Cell(x, y, true, Cell.WALL_FILE)
+      }
+      if(x == MAP_SIZE-1) {
+        y += 1
+        x = 0
+      } else x+=1
+    }
+  }
 }
